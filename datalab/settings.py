329c1a242f9a12094e4842f722523e4b7f5c3b24
Matt Daily <mdaily@lco.global>
2024-02-06 14:33:42 -0800

    Add dramatiq and settings

--- datalab/settings.py
+++ datalab/settings.py
@@ -60,10 +60,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'corsheaders',
-    'django_extensions',  # for debuging: shell_plus management command
+    'django_extensions',  # for debugging: shell_plus management command
     'rest_framework',
     'django_filters',
     'rest_framework.authtoken',
+    'django_dramatiq',
     'ocs_authentication.auth_profile',
     'datalab.datalab_session',
 ]
@@ -98,6 +99,36 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'datalab.wsgi.application'
+
+DRAMATIQ_BROKER = {
+    'BROKER': 'dramatiq.brokers.rabbitmq.RabbitmqBroker',
+    'OPTIONS': {
+        'url': os.getenv('DRAMATIQ_BROKER', 'amqp://localhost:5672'),
+    },
+    'MIDDLEWARE': [
+        'dramatiq.middleware.Prometheus',
+        'dramatiq.middleware.AgeLimit',
+        'dramatiq.middleware.TimeLimit',
+        'dramatiq.middleware.Callbacks',
+        'dramatiq.middleware.Retries',
+        'django_dramatiq.middleware.DbConnectionsMiddleware',
+        'django_dramatiq.middleware.AdminMiddleware',
+    ]
+}
+
+DRAMATIQ_RESULT_BACKEND = {
+    'BACKEND': 'dramatiq.results.backends.redis.RedisBackend',
+    'BACKEND_OPTIONS': {
+        'url': os.getenv('DRAMATIQ_RESULT_BACKEND', 'redis://localhost:6379'),
+    },
+    'MIDDLEWARE_OPTIONS': {
+        'result_ttl': 1000 * 60 * 10
+    }
+}
+
+# Defines which database should be used to persist Task objects when the
+# AdminMiddleware is enabled.  The default value is 'default'.
+DRAMATIQ_TASKS_DATABASE = 'default'
 
 
 # Database
