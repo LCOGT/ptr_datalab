dc4c51eabe309e879c7672d48702d4ef45972e85
Lloyd Dakin <taidakin99lol@gmail.com>
2024-03-29 15:44:30 -0700

    thumbnail service no zscaling

--- datalab/datalab_session/data_operations/median.py
+++ datalab/datalab_session/data_operations/median.py
@@ -5,9 +5,10 @@ import tempfile
 
 import numpy as np
 from astropy.io import fits
+from PIL import Image
 
 from datalab.datalab_session.data_operations.data_operation import BaseDataOperation
-from datalab.datalab_session.util import store_fits_output, get_archive_from_basename
+from datalab.datalab_session.util import add_file_to_bucket, get_archive_from_basename, numpy_to_thumbnails
 
 log = logging.getLogger()
 log.setLevel(logging.INFO)
@@ -45,9 +46,16 @@ The output is a median image for the n input images. This operation is commonly 
     def operate(self):
         input_files = self.input_data.get('input_files', [])
         file_count = len(input_files)
+        cache_key = self.generate_cache_key()
 
+        # Operation validation
         if file_count == 0:
+            log.warning(f'Tried to execute median operation on {file_count} files')
             return { 'output_files': [] }
+        # If cache key is already in S3 we already ran this operation on these inputs
+        if False:
+            # TODO: add a bucket check for cache key and then return files
+            pass
 
         log.info(f'Executing median operation on {file_count} files')
 
@@ -63,7 +71,7 @@ The output is a median image for the n input images. This operation is commonly 
                 except IndexError:
                     continue
 
-                with fits.open(fits_url, use_fsspec=True) as hdu_list:
+                with fits.open(fits_url) as hdu_list:
                     data = hdu_list['SCI'].data
                     memmap_path = os.path.join(temp_dir, f'memmap_{index}.dat')
                     memmap_array = np.memmap(memmap_path, dtype=data.dtype, mode='w+', shape=data.shape)
@@ -85,20 +93,23 @@ The output is a median image for the n input images. This operation is commonly 
             # Calculate a Median along the z axis
             median = np.median(stacked_data, axis=2)
 
-            cache_key = self.generate_cache_key()
+            # Create thumbnails
+            jpgs = numpy_to_thumbnails(median)
+
+            # Create the Fits File
             header = fits.Header([('KEY', cache_key)])
             primary_hdu = fits.PrimaryHDU(header=header)
             image_hdu = fits.ImageHDU(median)
             hdu_list = fits.HDUList([primary_hdu, image_hdu])
 
-            fits_buffer = BytesIO()
-            hdu_list.writeto(fits_buffer)
-            fits_buffer.seek(0)
+            # Save Fits and Thumbnails in S3 Buckets
+            add_file_to_bucket((cache_key + '/' + cache_key + '.fits'), hdu_list, 'FITS')
+            add_file_to_bucket((cache_key + '/' + cache_key + '-large.jpg'), jpgs['full'], 'JPEG')
+            add_file_to_bucket((cache_key + '/' + cache_key + '-small.jpg'), jpgs['thumbnail'], 'JPEG')
 
-            # Write the HDU List to the output FITS file in the bucket
-            response = store_fits_output(cache_key, fits_buffer)
+            # TODO: Get presigned urls for the jpgs and add to output
 
-        # TODO: No output yet, need to build a thumbnail service
+        # TODO: Return presigned urls as output
         output = {'output_files': []}
         self.set_percent_completion(file_count / file_count)
         self.set_output(output)
--- datalab/datalab_session/util.py
+++ datalab/datalab_session/util.py
@@ -1,25 +1,45 @@
 import requests
 import logging
+from io import BytesIO
 
 import boto3
+from PIL import Image
+import numpy as np
+from fits2image.conversions import fits_to_jpg
 
 from django.conf import settings
 
 log = logging.getLogger()
 log.setLevel(logging.INFO)
 
-def store_fits_output(item_key: str, fits_buffer: object) -> object:
+def add_file_to_bucket(item_key: str, file: object, file_format:str) -> object:
   """
   Stores a fits into the operation bucket in S3
 
   Keyword Arguements:
   item_key -- name under which to store the fits file
-  fits_buffer -- the fits file to add to the bucket
+  fits_buffer -- the fits file in a BytesIO buffer to add to the bucket
   """
   log.info(f'Adding {item_key} to {settings.DATALAB_OPERATION_BUCKET}')
 
-  s3 = boto3.resource('s3')
-  response = s3.Bucket(settings.DATALAB_OPERATION_BUCKET).put_object(Key = item_key, Body = fits_buffer.getvalue())
+  buffer = BytesIO()
+
+  if file_format == 'JPEG':
+    file.convert('RGB').save(buffer, format=file_format)
+  elif file_format == 'FITS':
+    file.writeto(buffer)
+  else:
+    log.error(f'Unknown file format {format}')
+    raise ValueError(f'add_file_to_bucket cant process {format} files')
+
+  buffer.seek(0)
+
+  s3 = boto3.client('s3')
+  response = s3.upload_fileobj(
+    buffer,
+    settings.DATALAB_OPERATION_BUCKET,
+    item_key
+  )
   return response
 
 def get_archive_from_basename(basename: str) -> dict:
@@ -41,3 +61,25 @@ def get_archive_from_basename(basename: str) -> dict:
     raise FileNotFoundError
 
   return results
+
+def numpy_to_thumbnails(img_arr: np.ndarray) -> dict:
+  """
+  Transforms a 2D numpy array into full res and thumbnail jpg image
+
+  Keyword Arguements:
+  basename -- name to query
+  """
+  THUMBNAIL_HEIGHT = 256
+  THUMBNAIL_WIDTH = 256
+  try:
+    log.info(f'img_arr dtype: {img_arr.dtype}')
+    log.info(f'img_arr shape: {img_arr.shape}')
+    full_size_img = Image.fromarray(img_arr, mode='F')
+    log.info(full_size_img.mode)
+    full_size_img.show()
+    thumbnail_img = full_size_img.copy()
+    thumbnail_img.thumbnail((THUMBNAIL_WIDTH, THUMBNAIL_HEIGHT))
+    return {'full': full_size_img, 'thumbnail': thumbnail_img}
+  except:
+    log.error(f'Failed to convert array {img_arr.shape} to jpgs')
+    raise OSError
