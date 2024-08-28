95ef17883c18c3cbbc4ee4688acca8e61acbabac
Lloyd Dakin <taidakin99lol@gmail.com>
2024-08-27 17:07:33 -0700

    TODOs for analysis

--- datalab/datalab_session/tests/test_analysis.py
+++ datalab/datalab_session/tests/test_analysis.py
@@ -6,11 +6,23 @@ class TestAnalysis(TestCase):
     def setUp(self):
         pass
 
-    def test_sample_analysis(self):
+    def test_get_tif(self):
+        # TODO use a test fits file to create a tif file
+
+        # TODO mock the get_s3_url call
+        # TODO mock the get_fits call
+        # TODO mock the add_file_to_bucket call
+
+        # TODO assert tif file exists
+        # TODO assert tif file exists pregenerated tif file
         pass
 
-    def test_sample_analysis_with_mock(self):
+    def test_line_profile(self):
+        # TODO use a test fits file that doesn't have a WCS header
+
+        # TODO assert that we throw a WCS error if the header is invalid
         pass
 
-    def test_sample_analysis_with_mock_and_exception(self):
+    def test_source_catalog(self):
+        # TODO use a test fits file 
         pass
