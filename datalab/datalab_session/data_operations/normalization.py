9a5d3fcaf1cc5ba2909a1d1c0023c2741d9e016f
Lloyd Dakin <taidakin99lol@gmail.com>
2024-09-16 12:17:27 -0700

    adjust percentages, remove logging messages

--- datalab/datalab_session/data_operations/normalization.py
+++ datalab/datalab_session/data_operations/normalization.py
@@ -20,7 +20,7 @@ class Normalization(BaseDataOperation):
     def description():
         return """The normalize operation takes in 1..n input images and calculates each image's median value and divides every pixel by that value.
 
-The output is a normalized image for the n input images. This operation is commonly used as a precursor step for flat removal."""
+The output is a normalized image. This operation is commonly used as a precursor step for flat removal."""
 
     @staticmethod
     def wizard_description():
@@ -43,23 +43,24 @@ The output is a normalized image for the n input images. This operation is commo
 
         input = self.input_data.get('input_files', [])
 
-        log.info(f'Executing normalization operation on {len(input)} file(s) {input}')
+        log.info(f'Executing normalization operation on {len(input)} file(s)')
 
-        image_data_list = self.get_fits_npdata(input, percent=0.4, cur_percent=0.0)
-        log.info(f'image data list: {image_data_list}')
+        image_data_list = self.get_fits_npdata(input)
+        self.set_percent_completion(0.40)
+
         output_files = []
-        for i, image in enumerate(image_data_list):
+        for index, image in enumerate(image_data_list):
             median = np.median(image)
             normalized_image = image / median
+
             fits_file = create_fits(self.cache_key, normalized_image)
-            log.info(f'fits_file: {fits_file}')
             large_jpg_path, small_jpg_path = create_jpgs(self.cache_key, fits_file)
-            output_file = save_fits_and_thumbnails(self.cache_key, fits_file, large_jpg_path, small_jpg_path, index=i)
+            output_file = save_fits_and_thumbnails(self.cache_key, fits_file, large_jpg_path, small_jpg_path, index=index)
             output_files.append(output_file)
-            log.info(f'x: {output_files}')
+
+            self.set_percent_completion(self.get_percent_completion() + .40 * (index + 1) / len(input))
             
         output =  {'output_files': output_files}
 
-        self.set_percent_completion(1.0)
         self.set_output(output)
         log.info(f'Normalization output: {self.get_output()}')
