048d59b6cd5a0839c23c734eb41e063364b8ebf0
Lloyd Dakin <taidakin99lol@gmail.com>
2024-09-16 11:55:45 -0700

    replace stack arrays with crop arrays, add index to basename for multiple outputs, change subtraction max to 999, update messages, update progress, crop subtraction image and input image, update test

--- datalab/datalab_session/data_operations/subtraction.py
+++ datalab/datalab_session/data_operations/subtraction.py
@@ -4,7 +4,7 @@ import numpy as np
 
 from datalab.datalab_session.data_operations.data_operation import BaseDataOperation
 from datalab.datalab_session.exceptions import ClientAlertException
-from datalab.datalab_session.file_utils import create_fits, create_jpgs
+from datalab.datalab_session.file_utils import create_fits, create_jpgs, crop_arrays
 from datalab.datalab_session.s3_utils import save_fits_and_thumbnails
 
 log = logging.getLogger()
@@ -36,7 +36,7 @@ class Subtraction(BaseDataOperation):
                     'description': 'The input files to operate on',
                     'type': 'file',
                     'minimum': 1,
-                    'maximum': 10
+                    'maximum': 999
                 },
                 'subtraction_file': {
                     'name': 'Subtraction File',
@@ -51,35 +51,40 @@ class Subtraction(BaseDataOperation):
     def operate(self):
 
         input_files = self.input_data.get('input_files', [])
+        print(f'Input files: {input_files}')
         subtraction_file_input = self.input_data.get('subtraction_file', [])
+        print(f'Subtraction file: {subtraction_file_input}')
 
         if not subtraction_file_input:
-            raise ClientAlertException('subtraction file not specified')
+            raise ClientAlertException('Missing a subtraction file')
 
         if len(input_files) < 1:
-            raise ClientAlertException(' needs at least 1 file')
+            raise ClientAlertException('Need at least one input file')
 
         log.info(f'Executing subtraction operation on {len(input_files)} files')
 
         input_image_data_list = self.get_fits_npdata(input_files)
-        self.set_percent_completion(.05)
+        self.set_percent_completion(.30)
 
         subtraction_image = self.get_fits_npdata(subtraction_file_input)[0]
-        self.set_percent_completion(.10)
+        self.set_percent_completion(.40)
 
         outputs = []
         for index, input_image in enumerate(input_image_data_list):
-            o = np.subtract(input_image, subtraction_image)
-            fits_file = create_fits(self.cache_key, o)
+            # crop the input_image and subtraction_image to the same size
+            input_image, subtraction_image = crop_arrays([input_image, subtraction_image])
+
+            difference_array = np.subtract(input_image, subtraction_image)
+
+            fits_file = create_fits(self.cache_key, difference_array)
             large_jpg_path, small_jpg_path = create_jpgs(self.cache_key, fits_file)
+
             output_file = save_fits_and_thumbnails(self.cache_key, fits_file, large_jpg_path, small_jpg_path, index)
             outputs.append(output_file)
 
-
-        self.set_percent_completion(.90)
+            self.set_percent_completion(self.get_percent_completion() + .50 * (index + 1) / len(input_files))
 
         output =  {'output_files': outputs}
 
-        self.set_percent_completion(1.0)
         self.set_output(output)
         log.info(f'Subtraction output: {self.get_output()}')
