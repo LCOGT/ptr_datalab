4f7acd58b5772a8fab8842380d4e8a057369e5a4
Jon <jnation@lco.global>
2024-11-05 02:57:37 -0100

    Fix the broken test and add a test_settings.py that is useful for running the tests locally without redis

--- datalab/datalab_session/analysis/raw_data.py
+++ datalab/datalab_session/analysis/raw_data.py
@@ -4,6 +4,8 @@ from datalab.datalab_session.s3_utils import get_fits
 from datalab.datalab_session.file_utils import get_hdu
 from fits2image.scaling import extract_samples, calc_zscale_min_max
 
+# TODO: This analysis endpoint assumes the image to be of 16 bitdepth. We should make this agnositc to bit depth in the future
+
 def raw_data(input: dict):
     fits_path = get_fits(input['basename'], input.get('source', 'archive'))
 
@@ -15,8 +17,8 @@ def raw_data(input: dict):
     median = np.median(samples)
     _, zmax, _ = calc_zscale_min_max(samples, contrast=0.1, iterations=1)
 
-    # resize the image to max. 500 pixels on an axis
-    max_size = input.get('max_size', 800)
+    # resize the image to max. 500 pixels on an axis by default for the UI
+    max_size = input.get('max_size', 500)
     image = Image.fromarray(image_data)
     newImage = image.resize((max_size, max_size), Image.LANCZOS)
     scaled_array = np.asarray(newImage).astype(np.float16)
