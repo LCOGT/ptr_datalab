ddc1e39126bb9fc548a25b71c685909fa455d70c
Lloyd Dakin <taidakin99lol@gmail.com>
2024-12-02 11:31:52 -0800

    source catalog and line profile use source for images, handle missing ra, dec, add clientAlertExceptions to analysis endpoint

--- datalab/datalab_session/analysis/source_catalog.py
+++ datalab/datalab_session/analysis/source_catalog.py
@@ -3,11 +3,28 @@ import numpy as np
 from datalab.datalab_session.file_utils import get_hdu, scale_points
 from datalab.datalab_session.s3_utils import get_fits
 
+# Source catalog Function Definition
+# ARGS: input (dict)
+#   input = {
+#     basename (str): The name of the file to analyze
+#     height (int): The height of the image
+#     width (int): The width of the image
+#     source (str): The source of the file
+#   }
+# RETURNS: output (dict)
+#   output = [{
+#     x (int): The x coordinate of the source
+#     y (int): The y coordinate of the source
+#     flux (int): The flux value of the source
+#     ra (float): The right ascension of the source
+#     dec (float): The declination of the source
+#   }]
+# 
 def source_catalog(input: dict):
   """
     Returns a dict representing the source catalog data with x,y coordinates and flux values
   """
-  fits_path = get_fits(input['basename'])
+  fits_path = get_fits(input['basename'], input['source'])
 
   cat_hdu = get_hdu(fits_path, 'CAT')
   sci_hdu = get_hdu(fits_path, 'SCI')
@@ -19,23 +36,30 @@ def source_catalog(input: dict):
   x_points = cat_hdu.data["x"][:SOURCE_CATALOG_COUNT]
   y_points = cat_hdu.data["y"][:SOURCE_CATALOG_COUNT]
   flux = cat_hdu.data["flux"][:SOURCE_CATALOG_COUNT]
-  ra = cat_hdu.data["ra"][:SOURCE_CATALOG_COUNT]
-  dec = cat_hdu.data["dec"][:SOURCE_CATALOG_COUNT]
+
+  # ra, dec values may or may not be present in the CAT hdu
+  if "ra" in cat_hdu.data.names and "dec" in cat_hdu.data.names:
+    ra = cat_hdu.data["ra"][:SOURCE_CATALOG_COUNT]
+    dec = cat_hdu.data["dec"][:SOURCE_CATALOG_COUNT]
+  else:
+    # TODO: implement a fallback way to calculate ra, dec from x, y and WCS
+    ra = np.zeros(SOURCE_CATALOG_COUNT)
+    dec = np.zeros(SOURCE_CATALOG_COUNT)
 
   # scale the x_points and y_points from the fits pixel coords to the jpg coords
   fits_height, fits_width = np.shape(sci_hdu.data)
   x_points, y_points = scale_points(fits_height, fits_width, input['width'], input['height'], x_points=x_points, y_points=y_points)
 
-  # we will be giving a list of dicts representing each source back to the frontend
+  # create the list of source catalog objects
   source_catalog_data = []
   for i in range(SOURCE_CATALOG_COUNT):
     source_catalog_data.append({
       "x": x_points[i],
       "y": y_points[i],
       "flux": flux[i].astype(int),
-      # truncate the ra and dec to 4 decimal places for readability
-      "ra": '%.4f'%(ra[i]),
-      "dec": '%.4f'%(dec[i])
+      # Astronomical coordinates are formatted to 6 decimal places
+      "ra": '%.6f'%(ra[i]),
+      "dec": '%.6f'%(dec[i])
     })
 
   return source_catalog_data
--- datalab/datalab_session/views.py
+++ datalab/datalab_session/views.py
@@ -1,3 +1,5 @@
+import logging
+
 from rest_framework.generics import RetrieveAPIView
 from rest_framework.renderers import JSONRenderer
 from rest_framework.response import Response
@@ -7,6 +9,7 @@ from datalab.datalab_session.analysis.line_profile import line_profile
 from datalab.datalab_session.analysis.source_catalog import source_catalog
 from datalab.datalab_session.analysis.get_tif import get_tif
 from datalab.datalab_session.analysis.raw_data import raw_data
+from datalab.datalab_session.exceptions import ClientAlertException
 
 
 class OperationOptionsApiView(RetrieveAPIView):
@@ -26,18 +29,32 @@ class AnalysisView(RetrieveAPIView):
         To add a new analysis action, add a case to the switch statement and create a new file in the analysis directory
     """
     def post(self, request, action):
-        input = request.data
+        log = logging.getLogger()
+        log.setLevel(logging.INFO)
 
-        match action:
-            case 'line-profile':
-                output = line_profile(input)
-            case 'source-catalog':
-                output = source_catalog(input)
-            case 'get-tif':
-                output = get_tif(input)
-            case 'raw-data':
-                output = raw_data(input)
-            case _:
-                raise Exception(f'Analysis action {action} not found')
+        try:
+            # TODO replace switch case with an AnalysisAction class
+            # AnalysisAction Class Definition
+            # class AnalysisAction:
+            #     def __init__(self, action):
+            #     def run(self, input):
+            #     def name(self):
+            input = request.data
 
-        return Response(output)
+            match action:
+                case 'line-profile':
+                    output = line_profile(input)
+                case 'source-catalog':
+                    output = source_catalog(input)
+                case 'get-tif':
+                    output = get_tif(input)
+                case 'raw-data':
+                    output = raw_data(input)
+                case _:
+                    raise Exception(f'Analysis action {action} not found')
+
+            return Response(output)
+        
+        except ClientAlertException as error:
+            log.error(f"Error running analysis action {action}: {error}")
+            return Response({"error": f"Image doesn't support {action}"}, status=400)
