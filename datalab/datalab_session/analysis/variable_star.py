9d1021160bb84bfc07a4dcec6539e3681827e81d
Lloyd Dakin <taidakin99lol@gmail.com>
2025-06-03 11:22:11 -0700

    variable star mag light curve extraction ffsspec hdu option

--- datalab/datalab_session/analysis/variable_star.py
+++ datalab/datalab_session/analysis/variable_star.py
@@ -1,22 +1,63 @@
+import logging
+
+from datalab.datalab_session.utils.file_utils import get_hdu
+from datalab.datalab_session.utils.filecache import FileCache
+
+log = logging.getLogger()
+log.setLevel(logging.INFO)
+
 def variable_star(input: dict):
   """
   Function to perform variable star analysis on a given image.
-  Args:
-      input (dict): Input dictionary containing star ra/dec and basenames
+
+  input (dict): Input dictionary containing 
+    target_coords(dict): ra,dec coordinates for target star
+    images(list):
+      image(dict):
+        basename(str): Name of the image file to be analyzed
+        id(str): Unique identifier for the image
+        observation_date(str): Date of the observation
   """
 
-  # Input
+  coords = input.get("target_coords")
+  target_ra = coords.get("ra")
+  target_dec = coords.get("dec")
 
-  coords = input.get("coords")
-  basenames = input.get("basenames")
+  light_curve = []
 
-  print(coords.get("ra"), coords.get("dec"))
-  print(basenames)
+  # Loop through each image's catalog and extract the target source's mag/magerr for the light curve
+  for image in input.get("images"):
+    log.info(f"Processing image: {image.get('basename')} for variable star analysis...")
 
-  # Fetch CAT data for basenames from archive
+    file_path = FileCache().get_fits(image.get('basename'), input.get('source', 'archive'))
+    cat_hdu = get_hdu(file_path, extension='CAT')
+    
+    target_source = find_target_source(cat_hdu, target_ra, target_dec)
 
-  # Pass CAT data to astrosource
+    if target_source is None:
+      log.info(f"No matching source found for target coordinates: RA={target_ra}, DEC={target_dec} in image {image.get('basename')}")
+    else:
+      light_curve.append({
+        'mag': target_source['mag'],
+        'magerr': target_source['magerr'],
+        'observation_date': image.get("observation_date"),
+      })
 
-  # Perform variable star analysis using astrosource
+  return {
+    'target_coords': coords,
+    'light_curve': light_curve
+  }
 
-  # Return results
+def find_target_source(cat_hdu, target_ra, target_dec):
+  """
+  Find the source in the catalog relative to the target coordinates.
+  """
+  cat_data = cat_hdu.data
+  MATCH_PERCISION = 0.001
+
+  for source in cat_data:
+    target_ra = float(target_ra)
+    target_dec = float(target_dec)
+
+    if abs(source['ra'] - target_ra) <= MATCH_PERCISION and abs(source['dec'] - target_dec) <= MATCH_PERCISION:
+      return source
