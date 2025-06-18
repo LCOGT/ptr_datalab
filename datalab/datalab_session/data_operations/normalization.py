ef234c9a43b4b69c18aa697b78d2bc909a0867c0
Carolina Capetillo <carolinacapetilloc@gmail.com>
2025-06-18 14:58:25 -0700

    Uses superclass's _validate_inputs and _process_inputs to validate and process inputs

--- datalab/datalab_session/data_operations/normalization.py
+++ datalab/datalab_session/data_operations/normalization.py
@@ -7,13 +7,21 @@ from datalab.datalab_session.data_operations.input_data_handler import InputData
 from datalab.datalab_session.data_operations.data_operation import BaseDataOperation
 from datalab.datalab_session.data_operations.fits_output_handler import FITSOutputHandler
 from datalab.datalab_session.utils.format import Format
+from datalab.datalab_session.exceptions import ClientAlertException
 
 log = logging.getLogger()
 log.setLevel(logging.INFO)
 
 
 class Normalization(BaseDataOperation):
-    
+    MINIMUM_NUMBER_OF_INPUTS = 1
+    PROGRESS_MIDPOINT_OFFSET = 0.5
+    PROGRESS_STEPS = {
+        'INPUT_PROCESSING_PERCENTAGE_COMPLETION': 0.2,   # 20% after inputs are loaded/validated
+        'NORMALIZING_PERCENTAGE_COMPLETION': 0.9,        # 90% after normalization is done
+        'OUTPUT_PERCENTAGE_COMPLETION': 1.0              # 100% after output is saved
+    }
+
     @staticmethod
     def name():
         return 'Normalization'
@@ -42,23 +50,36 @@ The output is a normalized image. This operation is commonly used as a precursor
         }
 
     def operate(self, submitter: User):
-        input_list = self.input_data.get('input_files', [])
+        input_list = self._validate_inputs(input_key='input_files', minimum_inputs=self.MINIMUM_NUMBER_OF_INPUTS)
+        print(f'HELLO')
+        input_handlers = self._process_inputs(
+            submitter,
+            input_list,
+            input_processing_progress=self.PROGRESS_STEPS['INPUT_PROCESSING_PERCENTAGE_COMPLETION']
+        )
+        print(f'Input handlers xxxxxxxx: {input_handlers}')
+
         log.info(f'Normalization operation on {len(input_list)} file(s)')
 
         output_files = []
-        for index, input in enumerate(input_list, start=1):
-            with InputDataHandler(submitter, input['basename'], input['source']) as image:
-                self.set_operation_progress(0.9 * (index-0.5) / len(input_list))
-                median = np.median(image.sci_data)
-                normalized_image = image.sci_data / median
+        for index, image in enumerate(input_handlers, start=1):
+            self.set_operation_progress(self.PROGRESS_STEPS['NORMALIZING_PERCENTAGE_COMPLETION'] * (index - self.PROGRESS_MIDPOINT_OFFSET) / len(input_handlers))
+            median = np.median(image.sci_data)
+            normalized_image = image.sci_data / median
 
-                comment = f'Datalab Normalization on file {input_list[index-1]["basename"]}'
-                output = FITSOutputHandler(f'{self.cache_key}', normalized_image, self.temp, comment, data_header=image.sci_hdu.header.copy()).create_and_save_data_products(Format.FITS, index=index)
-                output_files.append(output)
-                self.set_output(output_files)
-                self.set_operation_progress(0.9 * index / len(input_list))
+            comment = f'Datalab Normalization on file {input_list[index-1]["basename"]}'
+            output = FITSOutputHandler(
+                f'{self.cache_key}',
+                normalized_image,
+                self.temp,
+                comment,
+                data_header=image.sci_hdu.header.copy()
+            ).create_and_save_data_products(Format.FITS, index=index)
+            output_files.append(output)
+            self.set_output(output_files)
+            self.set_operation_progress(self.PROGRESS_STEPS['NORMALIZING_PERCENTAGE_COMPLETION'] * index / len(input_handlers))
 
         log.info(f'Normalization output: {output_files}')
         self.set_output(output_files)
-        self.set_operation_progress(1.0)
+        self.set_operation_progress(self.PROGRESS_STEPS['OUTPUT_PERCENTAGE_COMPLETION'])
         self.set_status('COMPLETED')
