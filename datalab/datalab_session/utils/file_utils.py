23996f492dcc3e9035efddfb5922a4b09503fe18
Lloyd Dakin <taidakin99lol@gmail.com>
2025-07-07 16:03:20 -0700

    Remove comment

--- datalab/datalab_session/utils/file_utils.py
+++ datalab/datalab_session/utils/file_utils.py
@@ -128,7 +128,6 @@ def scale_points(height_1: int, width_1: int, height_2: int, width_2: int, x_poi
   x_points = np.array(x_points)
   y_points = np.array(y_points)
 
-  # Possibly introduces error since result is rounded to int from a smaller image to a larger one
   x_points = ((x_points / width_1) * width_2)
   y_points = ((y_points / height_1) * height_2)
 
