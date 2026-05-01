f0d9e2a89c7f8e7698b3d0f6262d299d479884cf
Carolina Capetillo <carolinacapetilloc@gmail.com>
2026-05-01 15:40:26 -0700

    uses light_curve operation

--- datalab/datalab_session/analysis/variable_star.py
+++ datalab/datalab_session/analysis/variable_star.py
@@ -2,12 +2,9 @@ import logging
 
 from django.contrib.auth.models import User
 from astropy.timeseries import LombScargle
-from astropy.time import Time
 import numpy as np
 
-from datalab.datalab_session.utils.file_utils import get_hdu
-from datalab.datalab_session.utils.filecache import FileCache
-from datalab.datalab_session.utils.flux_to_mag import flux_to_mag
+from datalab.datalab_session.data_operations.light_curve import light_curve
 
 log = logging.getLogger(__name__)
 log.setLevel(logging.INFO)
@@ -25,89 +22,22 @@ def variable_star(input: dict, user: User):
         observation_date(str): Date of the observation
   """
 
-  coords = input.get("target_coords")
-  target_ra = coords.get("ra")
-  target_dec = coords.get("dec")
+  light_curve_result = light_curve(input, user)
+  light_curve_data = light_curve_result["light_curve"]
 
-  light_curve = []
-  excluded_images = []
-  flux_fallback = False
-
-  # Loop through each image's catalog and extract the target source's mag/magerr for the light curve
-  for image in input.get("images", []):
-    basename = image.get('basename')
-
-    try:
-      file_path = FileCache().get_fits(basename, input.get('source', 'archive'), user)
-      cat_hdu = get_hdu(file_path, extension='CAT')
-    except Exception as e:
-      log.error(f"Error retrieving catalog for image {basename}: {e}")
-      excluded_images.append(basename)
-      continue
-
-    target_source = find_target_source(cat_hdu, target_ra, target_dec)
-    if target_source is None:
-      log.info(f"No source found matching target coordinates: RA={target_ra}, DEC={target_dec} in image {basename}")
-      excluded_images.append(basename)
-      continue
-
-    try:
-      mag = target_source['mag']
-      print(f'mag: {mag}')
-      magerr = target_source['magerr']
-    except KeyError as e:
-      # If mag or magerr is not present, fallback convert flux to mag
-      mag, magerr = flux_to_mag(target_source['flux'], target_source['fluxerr'])
-      print(f'flux mag: {mag}')
-      flux_fallback = True
-    except Exception as e:
-      log.warning(f"Invalid magnitude or magnitude error for target in image {basename}")
-      excluded_images.append(basename)
-      continue
-
-    light_curve.append({
-      'mag': mag,
-      'magerr': magerr,
-      'julian_date': Time(image.get("observation_date")).jd,
-      'observation_date': image.get("observation_date")
-    })
-    print(f'light_curve: {light_curve}')
-  
   try:
-    frequency, power, period, fap = calculate_period(light_curve)
+    frequency, power, period, fap = calculate_period(light_curve_data)
   except Exception as e:
     log.error(f"Error calculating period: {e}")
-    period, fap = 0, 0
+    frequency, power, period, fap = [], [], 0, 0
 
   return {
-    'target_coords': coords,
-    'light_curve': light_curve,
-    'flux_fallback': flux_fallback,
-    'excluded_images': excluded_images,
-    'period': period,
-    'fap': fap,
-    'frequency': frequency,
-    'power': power
+    **light_curve_result,
+    "period": period,
+    "fap": fap,
+    "frequency": frequency,
+    "power": power,
   }
-
-def find_target_source(cat_hdu, target_ra, target_dec):
-  """
-  Find the source in the catalog relative to the target coordinates.
-  """
-  cat_data = cat_hdu.data
-  MATCH_PRECISION = 0.001
-
-  if 'ra' not in cat_data.names or 'dec' not in cat_data.names:
-    log.warning("CAT data does not have ra or dec names!")
-    return None
-  for source in cat_data:
-    target_ra = float(target_ra)
-    target_dec = float(target_dec)
-
-    if abs(source['ra'] - target_ra) <= MATCH_PRECISION and abs(source['dec'] - target_dec) <= MATCH_PRECISION:
-      return source
-
-  return None
 
 def calculate_period(light_curve):
   """
--- datalab/datalab_session/data_operations/variable_star.py
+++ datalab/datalab_session/data_operations/variable_star.py
@@ -2,15 +2,12 @@ import logging
 
 import numpy as np
 from astropy.timeseries import LombScargle
-from astropy.time import Time
-import numpy as np
 from django.contrib.auth.models import User
 
 from datalab.datalab_session.data_operations.data_operation import BaseDataOperation
+from datalab.datalab_session.data_operations.light_curve import light_curve
 from datalab.datalab_session.exceptions import ClientAlertException
 from datalab.datalab_session.utils.format import Format
-from datalab.datalab_session.utils.file_utils import get_hdu
-from datalab.datalab_session.utils.filecache import FileCache
 
 log = logging.getLogger()
 log.setLevel(logging.INFO)
@@ -71,50 +68,24 @@ The output is the lightcurve and periodogram data for period stacking of the pho
         comment= f'Datalab Variable Star analysis for target ra {target_ra}, dec {target_dec} on images: {", ".join([image["basename"] for image in input_files])}'
         log.info(comment)
 
-        light_curve = []
-        excluded_images = []
-        flux_fallback = False
-
-        for index, image in enumerate(input_files, start = 1):
-            basename = image.get('basename')
-            filter = image.get('filter', image.get('primary_optical_element', 'None'))
-            try:
-                file_path = FileCache().get_fits(basename, image.get('source', 'archive'), submitter)
-                cat_hdu = get_hdu(file_path, extension='CAT')
-            except Exception as e:
-                log.error(f"Error retrieving catalog for image {basename}: {e}")
-                excluded_images.append(basename)
-                continue
-
-            self.set_operation_progress(VariableStar.PROGRESS_STEPS['DOWNLOADING_INPUT_FILES'] * (index / len(input_files)))
-
-            target_source = find_target_source(cat_hdu, target_ra, target_dec)
-
-            if target_source is None:
-                log.info(f"No source found matching coordinates: RA={target_ra}, DEC={target_dec} in image {basename}")
-                excluded_images.append(basename)
-                continue
-
-            try:
-                mag = target_source['mag']
-                magerr = target_source['magerr']
-            except Exception as e:
-                log.warning(f"No calibrated magnitude or magnitude error for source in image {basename}")
-                excluded_images.append(basename)
-                continue
-
-            light_curve.append({
-            'mag': mag,
-            'magerr': magerr,
-            'julian_date': Time(image.get("observation_date")).jd,
-            'observation_date': image.get("observation_date")
-            })
+        light_curve_result = light_curve(
+            self.input_data,
+            submitter,
+            allow_flux_fallback=False,
+            progress_callback=lambda progress: self.set_operation_progress(
+                VariableStar.PROGRESS_STEPS['DOWNLOADING_INPUT_FILES'] * progress
+            )
+        )
+        light_curve_data = light_curve_result['light_curve']
+        excluded_images = light_curve_result['excluded_images']
+        flux_fallback = light_curve_result['flux_fallback']
+        filter = input_files[0].get('filter', input_files[0].get('primary_optical_element', 'None'))
         
-        if len(light_curve) < VariableStar.MINIMUM_NUMBER_OF_INPUTS:
-            raise ClientAlertException(f'Only found extractable photometry data in {len(light_curve)} inputs - this is not enough to get a period. Try increasing your date range to get more input images and try again.')
+        if len(light_curve_data) < VariableStar.MINIMUM_NUMBER_OF_INPUTS:
+            raise ClientAlertException(f'Only found extractable photometry data in {len(light_curve_data)} inputs - this is not enough to get a period. Try increasing your date range to get more input images and try again.')
         
         try:
-            frequency, power, period, fap = calculate_period(light_curve)
+            frequency, power, period, fap = calculate_period(light_curve_data)
         except Exception as e:
             log.error(f"Error calculating period: {e}")
             period, fap = 0, 0
@@ -124,7 +95,7 @@ The output is the lightcurve and periodogram data for period stacking of the pho
         output = {
             'output_data': [
                 {
-                    'light_curve': light_curve,
+                    'light_curve': light_curve_data,
                     'flux_fallback': flux_fallback,
                     'excluded_images': excluded_images,
                     'source': source,
@@ -136,32 +107,11 @@ The output is the lightcurve and periodogram data for period stacking of the pho
                 }
             ]
         }
-        log.info(f'Variable Star output found best period of {period} with false alarm prob. of {fap} using {len(light_curve)} / {len(input_files)} images')
+        log.info(f'Variable Star output found best period of {period} with false alarm prob. of {fap} using {len(light_curve_data)} / {len(input_files)} images')
 
         self.set_output(output, is_raw=True)
         self.set_operation_progress(VariableStar.PROGRESS_STEPS['OUTPUT_PERCENTAGE_COMPLETION'])
         self.set_status('COMPLETED')
-
-
-def find_target_source(cat_hdu, target_ra, target_dec):
-  """
-  Find the source in the catalog relative to the target coordinates.
-  """
-  cat_data = cat_hdu.data
-  MATCH_PRECISION = 0.001
-
-  if 'ra' not in cat_data.names or 'dec' not in cat_data.names:
-    log.warning("CAT data does not have ra or dec names!")
-    return None
-  for source in cat_data:
-    target_ra = float(target_ra)
-    target_dec = float(target_dec)
-
-    if abs(source['ra'] - target_ra) <= MATCH_PRECISION and abs(source['dec'] - target_dec) <= MATCH_PRECISION:
-      return source
-
-  return None
-
 
 def calculate_period(light_curve):
   """
